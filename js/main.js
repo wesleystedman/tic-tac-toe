fb32021f81a2afb034d15f7ca5040d0263ca96a4
Wesley <wesleystedman@gmail.com>
2020-08-02 19:10:15 -0700

    Add initial js file, w/ consts, state vars, and a couple of stubbed functions

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*----- constants -----*/
+const colors = {
+    null: 'black',
+    player1: 'red',
+    player2: 'blue'
+};
+const winningStates = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+/*----- app's state (variables) -----*/
+let board;
+let turn;
+let winner;
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init();
+
+function init() {
+
+}
+
+function render() {
+    
+}
